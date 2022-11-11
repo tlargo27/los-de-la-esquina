6d44d261bf21b4f29709d37b2d7e36ce7a49d4d3
Maicol Correa <mjalzate11@gmail.com>
2022-11-11 17:31:47 -0500

    Commiit para incluir el backen del proyecto .

---
+++ PolizasVehiculos/PolizasVehiculos.Infraestructura/Mapeos/PerfilesMapeo.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PolizasVehiculos.Core.DTOs;
+using PolizasVehiculos.Core.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolizasVehiculos.Infraestructura.Mapeos
+{
+    public class PerfilesMapeo : Profile
+    {
+        public PerfilesMapeo() {
+            CreateMap<Pais, PaisDTO>().ReverseMap();
+            CreateMap<Seguridad, SeguridadDTO>().ReverseMap();
+        }
+    }
+}
---
+++ PolizasVehiculos/PolizasVehiculos.Infraestructura/Repositorios/UnidadDeTrabajo.cs
@@ -0,0 +1,41 @@
+﻿using PolizasVehiculos.Core.Entidades;
+using PolizasVehiculos.Core.Interfaces;
+using PolizasVehiculos.Infraestructura.Datos;
+using System.Threading.Tasks;
+
+namespace PolizasVehiculos.Infraestructura.Repositorios
+{
+    public class UnidadDeTrabajo : IUnidadDeTrabajo
+    {
+        private readonly DBSegurosContext _contexto;
+        private readonly IRepositorio<Pais> _paisRepositorio;
+        private readonly ISeguridadRepositorio _seguridadRepositorio;
+
+        public UnidadDeTrabajo(DBSegurosContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IRepositorio<Pais> PaisRepositorio => _paisRepositorio ?? new RepositorioBase<Pais>(_contexto);
+
+        public ISeguridadRepositorio SeguridadRepositorio => _seguridadRepositorio ?? new SeguridadRepositorio(_contexto);
+
+        public void Dispose()
+        {
+            if (_contexto != null) 
+            {
+                _contexto.Dispose();
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _contexto.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _contexto.SaveChangesAsync();
+        }
+    }
+}
